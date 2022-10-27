0631b24835b283c136f275280cd65628fd9f04f9
Lisa Munsy <stillailleurs@gmail.com>
2022-10-27 11:21:47 +0200

    Correct waitingLine; create occurences2 and reamainder

---
+++ occurences2.js
@@ -0,0 +1,40 @@
+const input = process.argv[2];
+
+const array = input.split(",", 10);
+
+const numbers = []; 
+
+for (let i = 0; i < array.length; i++) { 
+    const candidate = array[i]; 
+    const number =parseInt(candidate, 10);
+    numbers.push(number);
+};
+
+let numberRepeated = -1;
+let sequence = false;
+
+for (let i = 0; i < 3; i++) {
+    const tmp = numbers[i];
+    let count =1;
+    for (let j = i+1; j < 5; j++) {
+        if (numbers[j] == tmp) {
+            count++;
+        }
+        if (count >= 3) {
+            numberRepeated = tmp;
+            break;
+        }
+    }
+    count =1;
+    if (numbers[i+1] == tmp && numbers[i+2] == tmp) {
+        sequence = true;
+    }
+};
+
+if (sequence == true) {
+    console.log(`The number ${numberRepeated} appears 3 times in a row.`);
+} else if (numberRepeated != -1) {
+    console.log(`The number ${numberRepeated} appears 3 times.`);
+} else {
+    console.log("No number appears 3 times");
+};
--- waitingLine.js
+++ waitingLine.js
@@ -3,17 +3,16 @@ const string = process.argv[2];
 const firstNames = string.split(",", 10);
 
 let actualState = firstNames;
-let lastExited ="";
-let newState = [];
 
 for (let i = 0; i < (actualState.length); i++) {
     console.log("Actual state = "+actualState);
-    lastExited = actualState[0];
+    let lastExited = actualState[0];
     console.log("Last exited = "+lastExited);
+    let newState = [];
 
-    for (let j = 0; j < (actualState.length -1); j++) {
-        console.log
-          newState.push(actualState[j+1]);
+    for (let j = 1; j < (actualState.length); j++) {
+        let tmp = actualState[j];
+          newState.push(tmp);
     }
     newState.push("");
     console.log("New state = "+newState);
